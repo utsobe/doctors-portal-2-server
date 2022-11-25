bf395af0707bf8b71b8d7832095e20cea3dfb0c7
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-11-26 00:41:21 +0800

    available slot api and post booking api created

--- index.js
+++ index.js
@@ -16,13 +16,51 @@ async function run() {
     try {
         await client.connect();
         const servicesCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'May 11, 2022';
+
+            // step 1: get all services
+
+            const services = await servicesCollection.find().toArray();
+
+            // get the booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            res.send(bookings);
         })
+
+        /**
+         * API Naming Convention
+         * app.get('/booking')
+         * app.get('/booking/:id')
+         * app.post('/booking')
+         * app.patch('/booking/:id')
+         * app.delete('/booking/:id')
+         */
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        });
+
+
+
     }
     finally {
 
